49ba86d596c19645c72830970b32c1e4b1468b39
makersahil <sahilkguptaprivate@gmail.com>
2025-08-24 17:15:18 +0530

    Basic Features Working: Verfied

---
+++ info.py
@@ -0,0 +1,8 @@
+
+print("""
+    Usage: thingy [OPTIONS]
+            -h                  Display this usage message
+            -H hostname         Hostname to connect to
+""")
+
+
---
+++ practice.py
@@ -0,0 +1,12 @@
+def ask_ok(prompt, retries=4, reminder="Please try again!"):
+    while True:
+        reply = input(prompt)
+        if reply in {'y', 'ye', 'yes'}:
+            return True
+        if reply in {'n', 'no', 'nop', 'nope'}:
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('Invalid user response')
+        print(reminder)
+
