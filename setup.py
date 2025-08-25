6141cb6abd2550dbcb9830538d905248edab56b5
makersahil <sahilkguptaprivate@gmail.com>
2025-08-25 19:22:27 +0530

    Fixed permission issues while creating database

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="create-prisma-app",   # <--- package name (pip install name)
-    version="0.1.0",
+    version="0.4.0",
     packages=find_packages(),
     install_requires=[],
     entry_points={
